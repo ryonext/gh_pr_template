a1b69421b40870c5eb8c9a3d484b3f1fdf8a1b15
ryonext <ryonext.s@gmail.com>
2015-10-15 23:38:23 +0900

    Need to use observer, but it does not work yet.

--- content-script.js
+++ content-script.js
@@ -1,12 +1,35 @@
-var injectScript;
+//var injectScript;
+//
+//injectScript = function(file, node) {
+//  var s, th;
+//  th = document.getElementsByTagName(node)[0];
+//  s = document.createElement('script');
+//  s.setAttribute('type', 'text/javascript');
+//  s.setAttribute('src', file);
+//  return th.appendChild(s);
+//}
+//
+//injectScript(chrome.extension.getURL('/embeded-script.js'), 'body')
+//  var target = document.querySelector('body');
+//  var config = {
+//    childList: true,
+//    subtree: true
+//  };
+//
+//  var observer = new MutationObserver(function(mutations, self) {
+//alert("hoge");
+//alert(document.title);
+//    mutations.forEach(function(mutation) {
+//      if (mutation.type === 'childList') {
+//alert("hoge");
+//alert(document.title);
+//      }
+//    });
+//  });
+//
+//  observer.observe(target, config);
 
-injectScript = function(file, node) {
-  var s, th;
-  th = document.getElementsByTagName(node)[0];
-  s = document.createElement('script');
-  s.setAttribute('type', 'text/javascript');
-  s.setAttribute('src', file);
-  return th.appendChild(s);
-}
-
-injectScript(chrome.extension.getURL('/embeded-script.js'), 'body')
+var observer = new MutationObserver(function (mutations) {
+alert("hoge");
+});
+observer.observe(document.body, { childList: true, subtree: true });
