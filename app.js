c2859cb0b81ac3498869f23ecd2af39ba9a7a5bd
ryonext <ryonext.s@gmail.com>
2015-10-18 23:46:59 +0900

    Success to get event in pjax page.

--- app.js
+++ app.js
@@ -1,27 +1,7 @@
-function() {
-  alert("sushi");
-  var target = document.queryselector('body');
-  var config = {
-    childlist: true,
-    subtree: true
-  };
-
-  var observer = new mutationobserver(function(mutations, self) {
-  alert("sushi");
-    mutations.foreach(function(mutation) {
-  alert("sushi");
-      if (mutation.type === 'childlist') {
-  alert("sushi");
-        var elements = document.queryselectorall('.pulls-list-group li.list-group-item');
-
-  alert("sushi");
-        array.prototype.foreach.call(elements, function(element) {
-  alert("sushi");
-          unhighlighter(element, re, color, opacity);
-        });
-      }
-    });
+(function() {
+  var observer = new MutationObserver(function(mutations) {
+    alert("sushi");
   });
 
-  observer.observe(target, config);
-};
+  observer.observe(document.body, { childList: true, subtree: true });
+})();
