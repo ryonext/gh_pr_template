60b1a62fbf36793f98147e42f5312163de2f7258
ryonext <ryonext.s@gmail.com>
2015-10-19 23:39:58 +0900

    Distinguish PR page or not.

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 (function() {
   var observer = new MutationObserver(function(mutations) {
-    alert("sushi");
+    if ($("#pull_request_body").length > 0) {
+      console.log("1");
+    }
+    else {
+      console.log("2");
+    }
   });
 
   observer.observe(document.body, { childList: true, subtree: true });
