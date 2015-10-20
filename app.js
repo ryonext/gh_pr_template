5788ee5086645f9b434a8bc6cbd503e061b209ed
ryonext <ryonext.s@gmail.com>
2015-10-20 23:30:41 +0900

    Success to update pr message.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 (function() {
+  var flag = 0
   var observer = new MutationObserver(function(mutations) {
-    if ($("#pull_request_body").length > 0) {
+    if ($("#pull_request_body").length > 0 && flag == 0) {
       console.log("1");
+      flag = 1;
+      $("#pull_request_body").text("## Background\n\n## Changes\n\n## See also");
     }
     else {
       console.log("2");
