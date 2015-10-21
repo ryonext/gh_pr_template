468b031d600c26c55e9455e3e3392de3593571b0
ryonext <ryonext.s@gmail.com>
2015-10-21 21:32:52 +0900

    Remove debug code.

--- app.js
+++ app.js
@@ -2,12 +2,8 @@
   var flag = 0
   var observer = new MutationObserver(function(mutations) {
     if ($("#pull_request_body").length > 0 && flag == 0) {
-      console.log("1");
-      flag = 1;
+      flag = 1; // Avoid 2 times run.
       $("#pull_request_body").text("## Background\n\n## Changes\n\n## See also");
-    }
-    else {
-      console.log("2");
     }
   });
 
